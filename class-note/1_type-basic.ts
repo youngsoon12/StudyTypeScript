2e26a9e252d2796817f636aec1fb59de09952009
youngsoon12 <enejwk789@gmail.com>
2022-09-27 21:51:00 +0900

    20220927

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,27 @@
+// JS 문자열 선언
+var str = "hello";
+
+// TS 문자열 선언
+var str: string = "hello";
+let num: number = 10;
+let arr: Array<number> = [1, 2, 3];
+let heroes: Array<string> = ["Capt", "Thor", "Hulk"];
+let itemes: number[] = [1, 2];
+
+// TS 튜플
+let adress: [string, string, number] = ["gangnam", "pangyo", 4];
+
+// TS 객체
+let obj: object = {};
+// let person: object = {
+//   name: "capt",
+//   age: 100,
+// };
+
+let person: { name: string; age: number } = {
+  name: "thor",
+  age: 1000,
+};
+
+// TS 진위값
+let show: boolean = true;
